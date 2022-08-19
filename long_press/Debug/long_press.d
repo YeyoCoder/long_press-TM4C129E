d25c29513c1d39f6325e3dd495aa5f7389a4d055
Diego Olvera <39103825+YeyoCoder@users.noreply.github.com>
2022-08-19 14:23:14 -0500

    Add files via upload
    First commit

---
+++ long_press/Debug/long_press.d
@@ -0,0 +1,23 @@
+# FIXED
+
+long_press.obj: ../long_press.c
+long_press.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+long_press.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+long_press.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+long_press.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+long_press.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+long_press.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+long_press.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+long_press.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+long_press.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+
+../long_press.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
